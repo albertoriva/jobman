94a4faeaf6ae49d4dd810d91e793c6b3bf9d1b51
Alberto Riva <ariva@ufl.edu>
2019-04-22 10:37:59 -0400

    added

---
+++ jobman.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python
+
+import sys
+import time
+import os.path
+import subprocess as sp
+
+# Globals
+
+PROCESS_STATES = ["ready", "running", "done"]
+
+class Job(object):
+    name = ""
+    proc = None
+    pid = None
+    cmdline = ""
+    status = None
+    retcode = 0
+
+    def __init__(self, cmdline, name=None):
+        self.cmdline = cmdline
+        if name:
+            self.name = name
+        else:
+            self.name = cmdline.split(" ")[0]
+        self.retcode = 0
+        self.status = "ready"
+
+    def start(self):
+        self.proc = sp.Popen(self.cmdline, shell=True)
+        self.pid = self.proc.pid
+        self.status = "running"
+        return self.pid
+
+    def check(self):
+        r = self.proc.poll()
+        if r is None:
+            return False
+        self.status = "done"
+        self.retcode = r
+        return True
+    
+class JobMan(object):
+    jobs = []
+    delay = 1
+    maxjobs = 0
+    filenames = []
+    _njobs = 0
+    _nrunning = 0
+    _ndone = 0
+    _log = False
+    
+    def __init__(self, maxjobs=0, delay=1):
+        self.jobs = []
+        self.delay = delay
+        self.maxjobs = maxjobs
+        self._njobs = 0
+        self._nrunning = 0
+        self._ndone = 0
+
+    def usage(self):
+        sys.stderr.write("""jobman.py - Simple job manager
+
+Usage: jobman.py [options] [filenames...]
+
+This program executes the specified subprocesses, waiting for them to 
+terminate. The commands to be executed can be read from one or more files, 
+or from standard input, if no file argument is specified. Options:
+
+-d D | Poll proocesses every D seconds (default: {}).
+-m M | Run at most M concurrent processes (default: no limit).
+-l   | Enable logging (to stderr).
+
+When all jobs are terminated, the program writes two integer numbers to 
+standard output, separated by a tab: the total number of jobs executed,
+and the number of jobs that returned an exit status of 0. The return
+status of the jobman process is the highest exit status returned by any
+of the jobs. For example, if the program executes three jobs, one of 
+which returns an exit status of 3, then: 
+
+$ jobman jobs.txt
+3 2
+$ echo $?
+3
+
+""".format(self.delay))
+        return False
+        
+    def parseArgs(self, args):
+        if "-h" in args or "--help" in args:
+            return self.usage()
+        prev = ""
+        for a in args:
+            if prev == "-d":
+                self.delay = int(a)
+                prev = ""
+            elif prev == "-m":
+                self.maxjobs = int(a)
+                prev = ""
+            elif a == "-l":
+                self._log = True
+            elif a in ["-d", "-m"]:
+                prev = a
+            else:
+                if os.path.isfile(a):
+                    self.filenames.append(a)
+        return True
+
+    def initialize(self):
+        if self.filenames:
+            for f in self.filenames:
+                self.initFromFile(f)
+        else:
+            self.initFromStream(sys.stdin)    
+        
+    def log(self, string, *args):
+        if self._log:
+            sys.stderr.write(string.format(*args) + "\n")
+        
+    def addJob(self, job):
+        self._njobs += 1
+        job.name = str(self._njobs)
+        self.jobs.append(job)
+        self.log("Job #{}: {}", job.name, job.cmdline)
+
+    def initFromFile(self, filename):
+        with open(filename, "r") as f:
+            self.initFromStream(f)
+
+    def initFromStream(self, f):
+        for line in f:
+            line = line.rstrip()
+            if line:
+                self.addJob(Job(line))
+        self.log("{} jobs defined.", self._njobs)
+        
+    def hasRoom(self):
+        """Return True if there's room to start a new job."""
+        return self.maxjobs == 0 or self._nrunning < self.maxjobs
+
+    def startJob(self, job):
+        job.start()
+        self._nrunning += 1
+        
+    def run(self):
+        while True:
+            # Check if any running jobs are done
+            for j in self.jobs:
+                # self.log("Checking job #{}, {}", j.name, j.status)
+                if j.status == "running" and j.check():
+                    self.log("Job #{} terminated.", j.name)
+                    self._nrunning += -1
+                    self._ndone += 1
+                    self.log("Jobs running: {}", self._nrunning)
+
+            # Check if we have room to start new jobs and
+            # that we have new jobs that can be started
+            for j in self.jobs:
+                if j.status == "ready" and self.hasRoom():
+                    self.startJob(j)
+                    self.log("Job #{} started.", j.name)
+                    self.log("Jobs running: {}", self._nrunning)
+                    
+            if self._ndone == self._njobs:
+                self.log("All jobs terminated.")
+                break
+
+            time.sleep(self.delay)
+
+    def report(self):
+        nzero = 0
+        maxret = 0
+        for j in self.jobs:
+            if j.retcode == 0:
+                nzero += 1
+            elif j.retcode > maxret:
+                maxret = j.retcode
+
+        sys.stdout.write("{}\t{}\n".format(self._njobs, nzero))
+        sys.exit(maxret)
+        
+def test():
+    JM = JobMan(delay=5, maxjobs=2)
+    JM.addJob(Job("sleep 30"))
+    JM.addJob(Job("sleep 50"))
+    JM.addJob(Job("sleep 20"))
+    JM.addJob(Job("sleep 40"))
+    JM.addJob(Job("sleep 10"))
+    return JM
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    JM = JobMan()
+    if JM.parseArgs(args):
+        JM.initialize()
+        JM.run()
+        JM.report()
+    
